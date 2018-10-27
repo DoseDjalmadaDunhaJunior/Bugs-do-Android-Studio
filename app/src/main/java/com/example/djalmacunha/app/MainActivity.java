250ebd3a54cf4d70cc84dde3e997ac25cf65cfac
djalma <djalminhajr_cunha@hotmail.com>
2018-10-27 09:44:58 -0300

    atualizei para ficar mais em ponta

--- app/src/main/java/com/example/djalmacunha/app/MainActivity.java
+++ app/src/main/java/com/example/djalmacunha/app/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.djalmacunha.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText log;
+    private EditText code;
+    private Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log = (EditText)findViewById(R.id.loginB);
+        code = (EditText)findViewById(R.id.senhaB);
+        ok = (Button)findViewById(R.id.go);
+
     }
 }
